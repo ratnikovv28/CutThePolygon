34a6b919c10120496b049bc239af789e2b506a89
Владимир <vladimir-ratnikov2002@mail.ru>
2022-12-03 14:19:37 +0300

    Some changes

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
-var polygon = new Path.RegularPolygon(new Point(200, 300), 4, 100);
-polygon.strokeColor = 'blue';
-polygon.fullySelected = true;
+var polygon = new Path.RegularPolygon(new Point(200, 300), Math.floor(Math.random()*10), 150);
+polygon.strokeColor = 'black';
+polygon.fillColor = 'white';
 
 var shapesArray = [];
 shapesArray.push(polygon);
 
+var count = document.getElementsByClassName('count');
+
 function splitShape(path1, path2){
     var shapesArrayCopy = path1.slice(0);
     shapesArray = [];
+    var dividePath;
+    var intersections;
     for(var i = 0; i < shapesArrayCopy.length; i++){
-        var intersections = shapesArrayCopy[i].getIntersections(path2);
+        intersections = shapesArrayCopy[i].getIntersections(path2);
         if(intersections.length >= 2){
             var p1 = shapesArrayCopy[i].split(shapesArrayCopy[i].getNearestLocation(intersections[0].point));
             var p2 = shapesArrayCopy[i].split(shapesArrayCopy[i].getNearestLocation(intersections[1].point));
+            
             p1.closed = true;
             p2.closed = true;
-            console.log(p1.position.x)
-            console.log(p1.position.y)
-            console.log(p2.position.x)
-            console.log(p2.position.y)
+            p1.fillColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+            p2.fillColor = "#" + Math.floor(Math.random()*16777215).toString(16);
             shapesArray.push(Object.assign(p1));
             shapesArray.push(Object.assign(p2));
             path2.visible = false;
@@ -27,6 +30,8 @@ function splitShape(path1, path2){
             shapesArray.push(shapesArrayCopy[i])
         }
     }
+    vectors(shapesArray);
+    count[0].innerHTML = shapesArray.length;
 }
 
 var myPath;
---
+++ js/start_animation.js
@@ -0,0 +1,51 @@
+function generateRandomColorHex() {
+	return "#" + ("00000" + Math.floor(Math.random() * Math.pow(16, 6)).toString(16)).slice(-6);
+  }
+
+var color = generateRandomColorHex();
+// The amount of circles we want to make:
+var count = 100;
+
+// Create a symbol, which we will use to place instances of later:
+var path = new Path.Circle({
+	center: [0, 0],
+	radius: 10,
+	fillColor: color,
+	strokeColor: 'black'
+});
+
+var symbol = new Symbol(path);
+
+// Place the instances of the symbol:
+for (var i = 0; i < count; i++) {
+	// The center position is a random point in the view:
+	var center = Point.random() * view.size;
+	var placedSymbol = symbol.place(center);
+	placedSymbol.scale(i / count);
+}
+
+// The onFrame function is called up to 60 times a second:
+function onFrame(event) {
+	// Run through the active layer's children list and change
+	// the position of the placed symbols:
+	for (var i = 0; i < count; i++) {
+		var item = project.activeLayer.children[i];
+		
+		// Move the item 1/20th of its width to the right. This way
+		// larger circles move faster than smaller circles:
+		item.position.x += item.bounds.width / 20;
+
+		// If the item has left the view on the right, move it back
+		// to the left:
+		if (item.bounds.left > view.size.width) {
+			item.position.x = -item.bounds.width;
+		}
+	}
+}
+
+var gameTitle = document.getElementsByClassName('game__title');
+gameTitle[0].style.border = "10px solid " + color;
+var gameName = document.getElementsByClassName('game__name');
+gameName[0].style.color = color;
+var startButton = document.getElementsByClassName('start_game__button');
+startButton[0].style.backgroundColor = color;
